3b446d715d8b9c0417404e59e21f70f677a363be
Ryukote <ryukote@outlook.com>
2018-12-29 23:30:29 +0100

    LDAP authentication.

---
+++ LDAP/Models/Domain.cs
@@ -0,0 +1,9 @@
+﻿namespace LDAP.Models
+{
+    class Domain
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string DomainName { get; set; }
+    }
+}
---
+++ LDAP/Utilities/LDAP.cs
@@ -0,0 +1,34 @@
+﻿using LDAP.Models;
+using Novell.Directory.Ldap;
+
+namespace LDAP.Utilities
+{
+    class LDAP
+    {
+        public bool LDAPAuthAsync(Domain userLogin)
+        {
+            if (!string.IsNullOrEmpty(userLogin.Username) && !string.IsNullOrEmpty(userLogin.Password))
+            {
+                try
+                {
+                    using (var connection = new LdapConnection { SecureSocketLayer = false })
+                    {
+                        string fullDomain = $"{userLogin.Username}@{userLogin.DomainName}";
+
+                        connection.Connect(userLogin.DomainName, LdapConnection.DEFAULT_PORT);
+                        connection.Bind(fullDomain, userLogin.Password);
+
+                        return connection.Bound;
+                    }
+                }
+                catch (LdapException)
+                {
+                    return false;
+                }
+            }
+
+            else
+                return false;
+        }
+    }
+}
